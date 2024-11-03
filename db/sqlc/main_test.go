75379e94f9e43358ba608864156ad50ae60c3042
Yuwei Liang <yuweichris8888@gmail.com>
2024-11-03 20:27:04 +1100

    fix test port

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret1@localhost:5435/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret1@localhost:5432/simple_bank?sslmode=disable"
 )
 
 
